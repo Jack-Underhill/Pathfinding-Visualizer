5f1e6eb5d858ed2561ed30e385f9108c8fc0c8c2
Jack-Underhill <jack.underhill17@gmail.com>
2025-07-15 12:51:20 -0700

    Skip Tracking Personal IPs

--- netlify/functions/track-visit.mjs
+++ netlify/functions/track-visit.mjs
@@ -1,6 +1,8 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
 import { Redis } from "@upstash/redis"
 
+import { shouldSkipIP } from "./utils/shouldSkipIP";
+
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
@@ -8,24 +10,28 @@ const redis = new Redis({
 
 export default async (request, context) => {
   const sitePrefix = "pfv";
+
   try {
     const ip = request.headers.get("x-nf-client-connection-ip") || "unknown";
+    let counted = false;
 
-    const alreadyVisited = await redis.get(`${sitePrefix}:visited:${ip}`);
-    if(!alreadyVisited) {
-      await redis.incr(`${sitePrefix}:unique_visits`);
-      await redis.set(`${sitePrefix}:visited:${ip}`, "true", { ex: 60 * 60 * 24 });
+    if(!shouldSkipIP(ip)) {
+      const alreadyVisited = await redis.get(`${sitePrefix}:visited:${ip}`);
+      if(!alreadyVisited) {
+        await redis.incr(`${sitePrefix}:unique_visits`);
+        await redis.set(`${sitePrefix}:visited:${ip}`, "true", { ex: 60 * 60 * 24 });
+        counted = true;
+      }
     }
 
     const count = await redis.get(`${sitePrefix}:unique_visits`);
-
-    return new Response(JSON.stringify({ count }), {
+    return new Response(JSON.stringify({ count: count ?? 0, counted }), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
       },
-    })
+    });
   } catch (error) {
     return new Response(error.toString(), { status: 500 });
   }
